50be3319b3d8530fba4813ad8eaf97f9a4377b97
Trovarelli <jtneto1995@hotmail.com>
2023-12-03 20:35:48 -0300

    tentativa deploy numero 4

--- dist/utils/handleParams.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleParams = void 0;
-const handleParams = (params) => {
-};
-exports.handleParams = handleParams;
-//# sourceMappingURL=handleParams.js.map
