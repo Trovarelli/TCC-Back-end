d6f1ef1bc3fd1301f2d4ed8a0c7efd162c33eded
Trovarelli <jtneto1995@hotmail.com>
2023-12-03 21:12:15 -0300

    tentativa de deploy 7

--- dist/index.js
+++ dist/index.js
@@ -22,7 +22,7 @@ const cors = require('cors');
 const app = (0, express_1.default)();
 app.use(express_1.default.json({ limit: '50mb' }));
 app.use(cors({
-    origin: '*'
+    origin: 'https://tahr.vercel.app'
 }));
 app.get('/', (req, res) => {
     return res.send('Seja bem vinda a tahr API');
--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json({limit: '50mb'}))
 
 app.use(cors({
-    origin: '*'
+    origin: 'https://tahr.vercel.app'
 }))
 
 app.get('/', (req, res) => {
